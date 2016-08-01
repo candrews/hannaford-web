86f1cd5b293f8272aa23f55b4189684d8f21f08d
Craig Andrews <candrews@integralblue.com>
2016-08-01 16:28:50 -0400

    Make ManifestController only respond got GET requests

--- src/main/java/com/integralblue/hannaford/web/controller/ManifestController.java
+++ src/main/java/com/integralblue/hannaford/web/controller/ManifestController.java
@@ -2,10 +2,11 @@ package com.integralblue.hannaford.web.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 
 @Controller
 public class ManifestController {
-	@RequestMapping("manifest.html")
+	@RequestMapping(method=RequestMethod.GET,value="manifest.html")
 	public String manifest(){
 		return "manifest";
 	}
